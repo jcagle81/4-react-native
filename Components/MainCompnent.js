bb0bad084c549090f553ce51530ad756a2213bc7
jcagle81 <57734109+jcagle81@users.noreply.github.com>
2021-05-25 09:39:22 -0500

    favorites

--- Components/MainCompnent.js
+++ Components/MainCompnent.js
@@ -14,6 +14,7 @@ import { connect } from 'react-redux';
 import { fetchCampsites, fetchComments, fetchPromotions,
     fetchPartners } from '../redux/ActionCreators';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoritesComponent';
 
 const mapDispatchToProps = {
     fetchCampsites,
@@ -141,6 +142,29 @@ const ReservationNavigator = createStackNavigator(
     }
 );
 
+const FavoritesNavigator = createStackNavigator(
+    {
+        Favorites: { screen: Favorites }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='heart'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView 
@@ -174,6 +198,7 @@ const MainNavigator = createDrawerNavigator(
                 )
             }
         },
+        
         Directory: {
             screen: DirectoryNavigator,
             navigationOptions: {
@@ -202,6 +227,22 @@ const MainNavigator = createDrawerNavigator(
             )
         }
     },
+    
+    Favorites: {
+        screen: FavoritesNavigator,
+        navigationOptions: {
+            drawerLabel: 'My Favorites',
+            drawerIcon: ({tintColor}) => (
+                <Icon
+                    name='heart'
+                    type='font-awesome'
+                    size={24}
+                    color={tintColor}
+                />
+            )
+        }
+    },    
+
     About: {
             screen: AboutNavigator,
             navigationOptions: {
