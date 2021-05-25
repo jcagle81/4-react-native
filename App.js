477f9c3f0dfec487c7883660d241e6623435d056
jcagle81 <57734109+jcagle81@users.noreply.github.com>
2021-05-25 11:10:46 -0500

    end of week 3

--- App.js
+++ App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 import Main from './Components/MainCompnent';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './components/LoadingComponent';
 
-const store = ConfigureStore();
+const { persistor, store } = ConfigureStore();
 
 export default function App() {
-  return (
-    <Provider store={store}>
-          <Main />
-      </Provider>
+    return (
+        <Provider store={store}>
+            <PersistGate
+                loading={<Loading />}
+                persistor={persistor}>
+                <Main />
+            </PersistGate>
+        </Provider>
     );
-  }
+}
